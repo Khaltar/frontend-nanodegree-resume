fa866b3285ed79bc2299353dd070b4dd123e24df
Jose Pereira <jafcpereira@gmail.com>
2016-01-23 19:42:16 +0000

    Finish lesson 0 and half of 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%", "José Pereira");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Web Developer");
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
