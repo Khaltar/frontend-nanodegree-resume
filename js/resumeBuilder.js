dbae96a44aef6e023b5029504bc99253dc61adc7
Jose Pereira <jafcpereira@gmail.com>
2016-01-23 23:57:26 +0000

    Finish lesson 3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -80,3 +80,67 @@ var education = {
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    for (var i = 0; i < bio.skills.length; i++){
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    }    
+}
+
+   
+
+var displayWork = function(){
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedLocation);
+        var formattedDates = HTMLworkStart.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }    
+}
+
+displayWork();
+
+
+var inName = function(name) {
+    var names = name.trim().split(" ");
+    var firstName = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    var lastName = names[1].toUpperCase();
+    var finalName = firstName + " " + lastName;
+    return finalName;                                                               
+}
+
+$("#main").append(internationalizeButton);
+
+projects.display = function() {
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+        
+        var formattedDates = HTMLprojectTitle.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+        
+        var formattedDescription = HTMLprojectTitle.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+        
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+
+projects.display()
+
+$("#mapDiv").append(googleMap);
